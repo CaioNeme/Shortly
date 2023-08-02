18512cc9a0d76a4ff32b14d56d7a3e967a79e03d
Caio Neme <caiocesarneme@gmail.com>
2023-08-02 12:22:29 -0300

    FEAT: LOGIN NA ROTA '/signin'

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -1,5 +1,6 @@
 import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 export async function signUp(req, res) {
   const { email, name, password, confirmPassword } = req.body;
@@ -11,7 +12,7 @@ export async function signUp(req, res) {
       email,
     ]);
     if (verification.rowCount > 0) {
-      return res.status(422).send("Esse email já esta sendo usado");
+      return res.status(409).send("Esse email já esta sendo usado");
     }
 
     const passwordHash = bcrypt.hashSync(password, 10);
@@ -27,7 +28,27 @@ export async function signUp(req, res) {
 }
 
 export async function signIn(req, res) {
+  const { email, password } = req.body;
+
   try {
+    const verification = await db.query(`SELECT * FROM users WHERE email=$1;`, [
+      email,
+    ]);
+    if (verification.rowCount != 1) {
+      return res.status(401).send("Dados incorretos");
+    }
+
+    if (bcrypt.compareSync(password, verification.rows[0].password) === false) {
+      return res.status(401).send("Dados incorretos");
+    }
+
+    const token = uuid();
+    await db.query(`INSERT INTO sessions (token, user_id) VALUES ($1, $2);`, [
+      token,
+      verification.rows[0].id,
+    ]);
+
+    res.status(200).send(token);
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/schemas/login.schemas.js
+++ src/schemas/login.schemas.js
@@ -1,7 +1,6 @@
 import joi from "joi";
 
 export const signInSchema = joi.object({
-  name: joi.string().required(),
   email: joi.string().required().email(),
   password: joi.string().required().min(3),
 });
