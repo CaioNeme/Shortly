cb2792d85fdbb484ffb363194b48bc72be2d9b59
Caio Neme <caiocesarneme@gmail.com>
2023-08-02 11:08:00 -0300

    Bug:database

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -1,8 +1,32 @@
-import { db } from "../database/database.connetcion.js";
+import { db } from "../database/database.connection.js";
+import bcrypt from "bcrypt";
 
 export async function signUp(req, res) {
+  // console.log(db);
+  const { email, name, password, confirmPassword } = req.body;
+  if (password != confirmPassword) {
+    return res.status(422).send("As senhas não coincidem");
+  }
   try {
+    const verification = await db.query(`SELECT * FROM users WHERE email=$1;`, [
+      email,
+    ]);
+    // console.log("1ª Querry ", verification);
+    if (verification.rowCount > 0) {
+      return res.status(422).send("Esse email já esta sendo usado");
+    }
+
+    const passwordHash = bcrypt.hashSync(password, 10);
+
+    const add = await db.query(
+      `INSERT INTO users (email, nome, password) VALUES ($1, $2, $3);`[
+        email, name, passwordHash
+      ]
+    );
+    console.log("2ª Querry ", add);
+    res.sendStatus(201);
   } catch (err) {
+    // console.log(err);
     res.status(500).send(err.message);
   }
 }
--- src/schemas/login.schemas.js
+++ src/schemas/login.schemas.js
@@ -1,5 +1,14 @@
-import Joi from "joi";
+import joi from "joi";
 
-export const signInSchema = Joi.object();
+export const signInSchema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().required().email(),
+  password: joi.string().required().min(3),
+});
 
-export const signUpSchema = Joi.object();
+export const signUpSchema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().required().email(),
+  password: joi.string().required().min(3),
+  confirmPassword: joi.string().required().min(3),
+});
