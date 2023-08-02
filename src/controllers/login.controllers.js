02f2b73704612259e5966c6379e4280e4e5ab480
Caio Neme <caiocesarneme@gmail.com>
2023-08-02 11:44:42 -0300

    FEAT: CADASTRO NA ROTA '/signup'

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -2,7 +2,6 @@ import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
 
 export async function signUp(req, res) {
-  // console.log(db);
   const { email, name, password, confirmPassword } = req.body;
   if (password != confirmPassword) {
     return res.status(422).send("As senhas não coincidem");
@@ -11,22 +10,18 @@ export async function signUp(req, res) {
     const verification = await db.query(`SELECT * FROM users WHERE email=$1;`, [
       email,
     ]);
-    // console.log("1ª Querry ", verification);
     if (verification.rowCount > 0) {
       return res.status(422).send("Esse email já esta sendo usado");
     }
 
     const passwordHash = bcrypt.hashSync(password, 10);
 
-    const add = await db.query(
-      `INSERT INTO users (email, nome, password) VALUES ($1, $2, $3);`[
-        email, name, passwordHash
-      ]
+    await db.query(
+      `INSERT INTO users (email, name, password) VALUES ($1, $2, $3);`,
+      [email, name, passwordHash]
     );
-    console.log("2ª Querry ", add);
     res.sendStatus(201);
   } catch (err) {
-    // console.log(err);
     res.status(500).send(err.message);
   }
 }
